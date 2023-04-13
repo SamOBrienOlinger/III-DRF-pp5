bd650fff76a996399a1ac2ac123ad32a5b118e6f
SamOBrienOlinger <samtobo@yahoo.co.uk>
2023-04-13 17:34:51 +0000

    Fix DATABASES

--- spoodle_backend/settings.py
+++ spoodle_backend/settings.py
@@ -163,16 +163,18 @@ WSGI_APPLICATION = 'spoodle_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': ({
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    } if 'DEV' in os.environ else dj_database_url.parse(
-        os.environ.get('DATABASE_URL')
-    ))
- 
-}
-print('connected')
+if 'DEV' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+    print('connected')
 
 # DATABASES = {
 #     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
